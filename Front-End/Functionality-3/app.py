a13491536c10147cfa17848a0b4f661433551b90
khushal088 <168260214+khushal088@users.noreply.github.com>
2024-12-04 17:37:11 +0530

    Update app.py

--- Front-End/Functionality-3/app.py
+++ Front-End/Functionality-3/app.py
@@ -4,10 +4,10 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the trained model
-model = load('random_forest_model.joblib')
 
-# Encoding maps for categorical inputs
+model = load(r"C:\Users\khushal\Desktop\completenew\joblib models\random_forest_model.joblib")
+
+
 encoding_map = {
     'GeneticMarkers': {'Positive': 1, 'Negative': 0},
     'Autoantibodies': {'Positive': 1, 'Negative': 0},
@@ -38,30 +38,30 @@ encoding_map = {
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+    return render_template('medical_form.html')
 
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
-        # Retrieve form data
+        
         form_data = request.form.to_dict()
 
-        # Encode categorical inputs
+        
         encoded_data = []
         for key, value in form_data.items():
             if key in encoding_map:
                 encoded_value = encoding_map[key][value]
                 encoded_data.append(encoded_value)
             else:
-                # Directly append numeric fields
+                
                 encoded_data.append(float(value))
 
-        # Convert data to numpy array for model prediction
+        
         input_array = np.array([encoded_data])
         prediction = model.predict(input_array)[0]
 
-        # Render prediction result
-        return render_template('result.html', prediction=prediction)
+        
+        return render_template('medical_result.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
