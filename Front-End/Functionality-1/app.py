c32d51247ca72c3d885445cb7539613b06f796fc
khushal088 <168260214+khushal088@users.noreply.github.com>
2024-12-04 17:32:49 +0530

    Update app.py

--- Front-End/Functionality-1/app.py
+++ Front-End/Functionality-1/app.py
@@ -2,32 +2,31 @@ from flask import Flask, request, render_template
 from joblib import load
 import numpy as np
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Load the saved model
-model = load('Functionality-1-LR.joblib')
+
+model = load(r'Functionality-1-LR.joblib')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('population.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get user input from the form
+        
         year = int(request.form['year'])
         
-        # Prepare data for the model (reshape as required by the model)
+        
         input_data = np.array([[year]])
         
-        # Predict using the model
+        
         prediction = model.predict(input_data)
         
-        # Return the prediction result to the frontend
-        return render_template('index.html', prediction=round(prediction[0]))
+        
+        return render_template('population.html', prediction=round(prediction[0]))
     except ValueError:
-        return render_template('index.html', error="Invalid input. Please enter a valid year.")
+        return render_template('templates\population.html', error="Invalid input. Please enter a valid year.")
 
 if __name__ == '__main__':
     app.run(debug=True)
